b193d2f3b417bc084b8838df83ca9f92d63de6a4
willdk3000 <w.doucetk@gmail.com>
2025-12-08 10:30:25 -0500

    layover avg added to inference data

--- regressions/controllers/predictions.js
+++ regressions/controllers/predictions.js
@@ -1,130 +1,3 @@
-//No variability
-
-`
-WITH base AS (
-    SELECT
-        date,
-        -- Extract the real block identifier before the underscore
-        (regexp_split_to_array(block_id, '_'))[1] AS block_key,
-        block_id,
-        trip_id,
-        route_id,
-		direction_id,
-        firstlast,
-        plannedduration,
-        realduration,
-        on_time_pct::int
-    FROM rtl.tripsreport where date between '2025-11-01' and '2025-11-30'
-	AND EXTRACT(DOW FROM date) IN (1,2,3,4,5) AND (service_id=4 OR service_id=8)
-),
-ordered AS (
-    SELECT
-        date,
-        block_key,
-        block_id,
-        trip_id,
-        route_id,
-		direction_id,
-        -- Convert planned start to seconds
-        firstlast[1]::int AS planned_start_sec,
-        plannedduration::int AS planned_dur_sec,
-        realduration::int AS real_dur_sec,
-        on_time_pct::int,
-
-        LEAD(trip_id) OVER (PARTITION BY date, block_key ORDER BY firstlast[1]::int) AS next_trip_id,
-        LEAD(route_id) OVER (PARTITION BY date, block_key ORDER BY firstlast[1]::int) AS next_route_id,
-		LEAD(direction_id) OVER (PARTITION BY date, block_key ORDER BY firstlast[1]::int) AS next_direction_id,
-        LEAD(firstlast[1]::int) OVER (PARTITION BY date, block_key ORDER BY firstlast[1]::int) AS next_planned_start_sec,
-        LEAD(plannedduration::int) OVER (PARTITION BY date, block_key ORDER BY firstlast[1]::int) AS next_planned_dur_sec,
-        LEAD(on_time_pct) OVER (PARTITION BY date, block_key ORDER BY firstlast[1]::int) AS next_on_time_pct
-    FROM base
-),
-pairs AS (
-    SELECT
-        date,
-        block_key,
-        block_id,
-
-        -- current trip
-        trip_id AS trip_a_id,
-        route_id AS route_a,
-		direction_id AS direction_a,
-		
-        on_time_pct AS on_time_a,
-        planned_start_sec AS planned_start_a,
-        planned_dur_sec AS planned_dur_a,
-
-        -- next trip
-        next_trip_id AS trip_b_id,
-        next_route_id AS route_b,
-		next_direction_id AS direction_b,
-        next_on_time_pct AS on_time_b,
-        next_planned_start_sec AS planned_start_b,
-        next_planned_dur_sec AS planned_dur_b,
-
-        -- layover in seconds
-        CASE
-            WHEN next_planned_start_sec IS NOT NULL THEN
-                next_planned_start_sec - (planned_start_sec + planned_dur_sec)
-        END AS planned_layover_sec,
-		planned_start_sec,
-		next_planned_start_sec
-    FROM ordered
-),
-d AS (
-SELECT *
-FROM pairs
-WHERE trip_b_id IS NOT NULL
-ORDER BY date, block_key, planned_start_a
-),
-r as (
-SELECT
-
-	CASE WHEN on_time_b >= 0.85 THEN 1 ELSE 0 END AS y_on_time_b,
-	CASE WHEN on_time_a >= 0.85 THEN 1 ELSE 0 END AS on_time_a,
-
-    planned_dur_a,
-    planned_dur_b,
-
-    CASE WHEN planned_layover_sec>0 THEN planned_layover_sec
-	ELSE 0
-	END as planned_layover_sec,
-
-	--engineered features
-	--planned_start_sec/3600 as start_hour_a,
-	--next_planned_start_sec/3660 as start_hour_b,
-
-	CASE WHEN planned_start_sec>21600 AND planned_start_sec<=32400
-	THEN 1 ELSE 0
-	END as ampeak_a,
-
-	CASE WHEN planned_start_sec>55800 AND planned_start_sec<=66600
-	THEN 1 ELSE 0
-	END as pmpeak_a,
-
-    CONCAT(route_a, '_', direction_a, '_', route_b, '_', direction_b) AS route_pair
-	
-FROM d 
-WHERE on_time_a IS NOT NULL and on_time_b IS NOT NULL
-AND planned_layover_sec < 900
-),
-r1 as (
-SELECT 
-distinct(route_pair) as route_pair
-FROM r
-)
-SELECT 
-0 as on_time_a,
-300 as planned_layover_sec,
-2400 as planned_dur_a,
-2400 as planned_dur_b,
-1 as ampeak_a,
-0 as pmpeak_a,
-route_pair
-from r1
-`
-
-
 //WITH variability
 `
 WITH orig AS (
@@ -263,14 +136,18 @@ AND planned_layover_sec < 900
 ),
 r1 as (
 SELECT 
-distinct(route_pair) as route_pair
+distinct(route_pair) as route_pair,
+avg(planned_layover_sec) as avg_layover_sec
 FROM r
+group by route_pair
 )
 SELECT 
 0 as on_time_a,
-300 as planned_layover_sec,
 1900 as planned_dur_b,
--230 as avg_var_b,
+0.971985 as p85_pct_b,
+241.93 as range7525_b,
+--300 as planned_layover_sec,
+avg_layover_sec as planned_layover_sec,
 0 as ampeak_a,
 1 as pmpeak_a,
 route_pair
